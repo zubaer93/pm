9de34ec249717d885e35533bd55e58dea09b75ee
Zubaer Ahmad <zahmad@leapinglogic.com>
2019-02-17 23:28:09 +0600

    check webhook

--- plugins/Api/src/Controller/AppController.php
+++ plugins/Api/src/Controller/AppController.php
@@ -36,6 +36,8 @@ class AppController extends RestApiController
        /* if (Configure::read('ApiRequest.jwtAuth.enabled')) {
             $this->manageToken($event);
         }*/
+
+
        return true;
     }
 
